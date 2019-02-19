35bc4e896fd1246d7038b5d6ae3f0defd0623f38
Yuui Tanabe <tanabeyuui721@icloud.com>
2019-02-19 18:35:39 +0900

    Initial Commit

---
+++ src/com/yuuitanabe/webbrowser/ReadFileMain.java
@@ -0,0 +1,10 @@
+package com.yuuitanabe.webbrowser;
+
+import javax.swing.JFrame;
+
+public class ReadFileMain {
+	public static void main(String[] args) {
+		ReadFile file = new ReadFile();
+		file.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
