66a107f805279db56474bc6c1396d3cb012b381c
Yuui Tanabe <yuuitanabe@163.com>
2019-02-28 21:38:52 +0900

    Update LibraryTest.java

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        Parsing classUnderTest = new Parsing();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.parse());
     }
 }
