c37598735f1b8adfb0a73f50f515957290de9db6
Yuui Tanabe <yuuitanabe@163.com>
2019-02-28 21:37:48 +0900

    Update Parsing.java

--- src/main/java/com/yuuitanabe/webbrowser/Parsing.java
+++ src/main/java/com/yuuitanabe/webbrowser/Parsing.java
@@ -6,7 +6,7 @@ package com.yuuitanabe.webbrowser;
 import org.jsoup.Jsoup;
 
 public class Parsing {
-    public static void main(String[] args) {
+    public static void parsing() {
     	
     }
 }
