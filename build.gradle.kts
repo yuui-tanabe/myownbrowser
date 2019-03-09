23f59bda6463bac512e95bcfea5c90c7712f1181
Yuui Tanabe <yuuitanabe@163.com>
2019-03-09 15:24:23 +0900

    [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@
 
 plugins {
     // Apply the java-library plugin to add support for Java Library & others
-    val plugins = listOf("java-library","eclipse")
+    val plugins = listOf("java-library","eclipse","org.sonarqube")
     plugins.forEach {
         id(it)
     }
