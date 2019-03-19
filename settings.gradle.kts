c38884a35e5942a5ffa51060d4ceea55293fdd62
Yuui Tanabe <yuitanabe@aol.com>
2019-03-19 20:08:17 +0900

    Change Project Name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user guide at https://docs.gradle.org/4.8.1/userguide/multi_project_builds.html
  */
 
-(rootProject as ProjectDescriptor).name = "ownbrowserlib"
+(rootProject as ProjectDescriptor).name = "pipres"
 (rootProject as ProjectDescriptor).buildFileName = "build.gradle.kts"
